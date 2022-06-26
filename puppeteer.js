9d1d291a0f89297db41b5a395f4f2d8cebebf572
evan <97516411+0x3van@users.noreply.github.com>
2022-06-26 17:50:03 +0800

    comedy

---
+++ puppeteer.js
@@ -0,0 +1,64 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+
+puppeteer.use(StealthPlugin());
+
+/**
+ * @type {import('puppeteer').Browser}
+ */
+let browser;
+/**
+ * @type {() => Promise<import('puppeteer').BrowserContext>}
+ */
+let context;
+
+async function init(i) {
+    if (!browser) {
+        browser = await puppeteer.launch({
+            headless: false,
+            userDataDir: './userData' + (i ? `-${i}` : ''),
+            args: [
+                '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"',
+                '--no-sandbox'
+            ],
+        })
+        const [page] = await browser.pages();
+
+        const session = await page.target().createCDPSession();
+        const { windowId } = await session.send('Browser.getWindowForTarget');
+        await session.send('Browser.setWindowBounds', { windowId, bounds: { windowState: 'minimized' } });
+    }
+    if (!context)
+        context = async () => await browser.createIncognitoBrowserContext();
+    return
+}
+
+async function page() {
+    const ctx = await context();
+
+    const page = await ctx.newPage();
+
+    await page.setViewport({
+        width: 1920,
+        height: 1080,
+    });
+
+    await page.evaluateOnNewDocument(() => {
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => false,
+        });
+    });
+
+    page.__close = page.close;
+    page.close = async () => {
+        await page.__close();
+        await ctx.close();
+    }
+
+    return page;
+}
+
+module.exports = {
+    init,
+    page
+};
---
+++ worker.js
@@ -0,0 +1,18 @@
+const gen = require('./generate')
+const puppeteer = require('./puppeteer');
+const {
+    workerData
+} = require('worker_threads')
+
+async function main() {
+    console.log('starting')
+    await gen.generateRepl('tonumber/Testing', 's%3AemE6pcfvOK6cRy7GaQxs5gB0OFBJ9qY9.wQvEzCgSMb1w8i3peXzMiH2XmA592xOm7GeXOZbK2tU')
+    console.log('made repl')
+    main()
+}
+
+puppeteer.init(workerData.id).then(() => {
+    for (let i = 0; i < workerData.count; i++) {
+        main()
+    }
+})
